b39742d557ea9082dcf1e22778174637c41fde21
Struth129 <sakets.ug18.ee@gmail.com>
2021-04-28 23:02:41 +0530

    UI done

--- app/src/main/java/com/example/cal_app/MainActivity.kt
+++ app/src/main/java/com/example/cal_app/MainActivity.kt
@@ -2,11 +2,22 @@ package com.example.cal_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onDigit(view: View){
+        tvInput.append((view as Button).text)
+    }
+
+    fun onClear(view: View){
+        tvInput.text = ""
     }
 }
