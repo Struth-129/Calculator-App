f8502c59b0ae84dca2ad635985405dbdaed32915
Struth129 <sakets.ug18.ee@gmail.com>
2021-04-29 23:46:02 +0530

    final commit

--- app/src/main/java/com/example/cal_app/MainActivity.kt
+++ app/src/main/java/com/example/cal_app/MainActivity.kt
@@ -9,15 +9,103 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var lastNumeric:Boolean = false
+    var lastDot: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
     fun onDigit(view: View){
         tvInput.append((view as Button).text)
+        lastNumeric = true
     }
 
     fun onClear(view: View){
         tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
     }
+
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
+    fun onEqual(view: View){
+        if(lastNumeric){
+            var tvValue = tvInput.text.toString()
+            var prefix = ""
+            try{
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+
+                }
+                if(tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(!prefix.isEmpty()){
+                        one = prefix+one
+                    }
+
+                    tvInput.text = (one.toDouble() - two.toDouble()).toString()
+                }
+                if(tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(!prefix.isEmpty()){
+                        one = prefix+one
+                    }
+
+                    tvInput.text = (one.toDouble() + two.toDouble()).toString()
+                }
+                if(tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(!prefix.isEmpty()){
+                        one = prefix+one
+                    }
+
+                    tvInput.text = (one.toDouble() / two.toDouble()).toString()
+                }
+                if(tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(!prefix.isEmpty()){
+                        one = prefix+one
+                    }
+
+                    tvInput.text = (one.toDouble() * two.toDouble()).toString()
+                }
+            }catch(e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun onOperator(view: View){
+        if(lastNumeric && !isOperatorAdded(tvInput.text.toString())){
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
+    }
+
+    private fun isOperatorAdded(value: String): Boolean{
+        return if(value.startsWith("-")){
+            false
+        }else{
+            value.contains("/")||value.contains("*")
+                    || value.contains("+")||value.contains("-")
+        }
+    }
+
 }
